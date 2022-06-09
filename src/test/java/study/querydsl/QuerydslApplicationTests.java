1fa5bb8f4520f5c8a6f59b98c77801604456542d
KDH_Notebook <destiny1017@nate.com>
2022-06-09 20:13:32 +0900

    예제 도메인 모델과 동작확인

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -8,15 +8,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
+import study.querydsl.entity.Member;
 import study.querydsl.entity.QHello;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
@@ -38,4 +41,30 @@ class QuerydslApplicationTests {
 		assertThat(hello1.getId()).isEqualTo(hello.getId());
 	}
 
+	@Test
+	public void testEntity() {
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+		//초기화
+		em.flush();
+		em.clear();
+		//확인
+		List<Member> members = em.createQuery("select m from Member m",
+						Member.class).getResultList();
+		for (Member member : members) {
+			System.out.println("member=" + member);
+			System.out.println("-> member.team=" + member.getTeam());
+		}
+	}
 }
+
