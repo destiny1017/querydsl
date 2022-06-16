2ab0e03a22396834ae7f8cbfd5de5f5c44024738
KDH_Notebook <destiny1017@nate.com>
2022-06-16 13:30:39 +0900

    동적쿼리와 성능 최적화 조회 - Where절 파라미터

--- src/test/java/study/querydsl/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/MemberJpaRepositoryTest.java
@@ -63,12 +63,12 @@ public class MemberJpaRepositoryTest {
         em.persist(member4);
 
         MemberSearchCondition condition = new MemberSearchCondition();
-//        condition.setAgeLoe(40);
-//        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setAgeGoe(35);
         condition.setTeamName("teamB");
 
-        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
 
-        assertThat(result).extracting("username").containsExactly("member3", "member4");
+        assertThat(result).extracting("username").containsExactly("member4");
     }
 }
