9a7cc0f93dce56aa1a306c43cffbefe2c05ed54a
KDH_Notebook <destiny1017@nate.com>
2022-06-17 20:12:46 +0900

    QuerydslPredicateExecutor

--- src/test/java/study/querydsl/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 import study.querydsl.repository.MemberJpaRepository;
 import study.querydsl.repository.MemberRepository;
@@ -108,6 +109,31 @@ public class MemberRepositoryTest {
         for (MemberTeamDto memberTeamDto : result.getContent()) {
             System.out.println("memberTeamDto = " + memberTeamDto);
         }
+    }
+
+    @Test
+    public void querydslPredicateExecutorTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        Iterable<Member> result = memberRepository.findAll(QMember.member.age.between(25, 40)
+                .and(QMember.member.username.eq("member3")));
+
+        for (Member member : result) {
+            System.out.println("member = " + member);
+        }
+
+        String a;
     }
 
 
